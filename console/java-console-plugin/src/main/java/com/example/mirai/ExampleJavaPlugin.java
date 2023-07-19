b0ac2b78694f64e83fbc9924bed378b1e2a40e56
cssxsh <cssxsh@gmail.com>
2023-07-19 21:07:19 +0800

    add: use Scheduler

--- console/java-console-plugin/src/main/java/com/example/mirai/ExampleJavaPlugin.java
+++ console/java-console-plugin/src/main/java/com/example/mirai/ExampleJavaPlugin.java
@@ -12,7 +12,22 @@ public class ExampleJavaPlugin extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // ...
+        // 异步任务
+        getScheduler().async(() -> {
+            getLogger().info("...");
+        });
+
+        // 延时任务
+        getScheduler().delayed(30_000, () -> {
+            getLogger().info("...");
+        });
+
+        // 定时任务
+        getScheduler().repeating(60_000, () -> {
+            getLogger().info("...");
+            getLogger().info("...");
+            getLogger().info("...");
+        });
     }
 
     @Override
