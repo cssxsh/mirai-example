53576cd55d9bd9957abba9aea2e2f7f69a689724
cssxsh <cssxsh@gmail.com>
2023-07-19 21:16:02 +0800

    add: use Scheduler

--- console/java-console-plugin/src/main/java/com/example/mirai/ExampleJavaPlugin.java
+++ console/java-console-plugin/src/main/java/com/example/mirai/ExampleJavaPlugin.java
@@ -1,7 +1,11 @@
 package com.example.mirai;
 
+import net.mamoe.mirai.Bot;
 import net.mamoe.mirai.console.plugin.jvm.JavaPlugin;
 import net.mamoe.mirai.console.plugin.jvm.JvmPluginDescriptionBuilder;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 
 public class ExampleJavaPlugin extends JavaPlugin {
     public ExampleJavaPlugin() {
@@ -17,17 +21,36 @@ public class ExampleJavaPlugin extends JavaPlugin {
             getLogger().info("...");
         });
 
+        // 异步任务
+        Future<Long> async = getScheduler().async(() -> System.currentTimeMillis() / 1000);
+
+        try {
+            getLogger().info(async.get().toString());
+        } catch (InterruptedException | ExecutionException e) {
+            throw new RuntimeException(e);
+        }
+
         // 延时任务
         getScheduler().delayed(30_000, () -> {
             getLogger().info("...");
         });
 
+        Future<Bot> delayed = getScheduler().delayed(30_000, () -> Bot.getInstance(12345));
+
+        try {
+            getLogger().info(delayed.get().toString());
+        } catch (InterruptedException | ExecutionException e) {
+            throw new RuntimeException(e);
+        }
+
         // 定时任务
-        getScheduler().repeating(60_000, () -> {
+        Future<Void> repeating = getScheduler().repeating(60_000, () -> {
             getLogger().info("...");
             getLogger().info("...");
             getLogger().info("...");
         });
+        // 结束定时任务
+        repeating.cancel(true);
     }
 
     @Override
